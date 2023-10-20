67f3b5551c903d8fcbec83bf5eccf7570ce5c0a9
Hidayat <tomilolaidayat@gmail.com>
2023-10-20 00:06:10 +0000

    Add figure.png

--- figure.R
+++ figure.R
@@ -4,5 +4,6 @@ library(cowplot)
 theme_set(theme_cowplot())
 set.seed(298347)
 dat <- tibble(x = rnorm(100), y = x + rnorm(100, sd = 0.2))
-ggplot(dat, aes(x,y)) +
+g1 <- ggplot(dat, aes(x,y)) +
   geom_point()
+ggsave('myscript.png',g1)
